1076529a3455c207add740ca75543d1ca1e83449
Peter Mazuryk <petermazuryk1984@gmail.com>
2019-10-17 00:01:45 +0300

    Updates

---
+++ precache-manifest.72f95be19555b08f6b28f07bf986eb59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953d2efa4ee308d0427277455b3ff944",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b471f404c873a16cb2de",
+    "url": "/goit-react-hw-01-components/static/css/main.37756277.chunk.css"
+  },
+  {
+    "revision": "3570be5916b85a8fb886",
+    "url": "/goit-react-hw-01-components/static/js/2.9d161150.chunk.js"
+  },
+  {
+    "revision": "b471f404c873a16cb2de",
+    "url": "/goit-react-hw-01-components/static/js/main.1157d3c1.chunk.js"
+  },
+  {
+    "revision": "939ae3c5a411a12b7c94",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1ee0e33e.js"
+  }
+]);
